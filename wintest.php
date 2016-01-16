c57c78ca9b59bfa4d79c63e40195204059cf89c9
cometCube <vijaykumar@osscube.com>
2016-01-17 00:42:21 +0530

    second test

--- wintest.php
+++ wintest.php
@@ -6,6 +6,7 @@ include "rdfl_connect.inc";
 </head>
 <body>
 <p>Hi This is Test.</p>
+<p>Hi This is Second Test.</p>
 <input type="button" value="RDFL" onclick="javascript:openwin('rdfltest.php');">
 <input type="button" value="Register Member" onclick="javascript:openwin('pooling_mem_reg.php');">
 <input type="button" value="Search Member" onclick="javascript:openwin('pooling_mem_srch.php');">
